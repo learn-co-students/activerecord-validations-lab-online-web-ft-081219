ac2f3415f7fa78f023868d009fcc2333ad598ed9
Armel <armelfli50@gmail.com>
2019-10-17 01:18:08 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ActiveRecord::Base
+  
+    validates :title, presence: true
+    validates :content, length: {minimum: 20}
+    validates :summary, length: {maximum: 50}
+    validates :category, inclusion: ["Fiction", "Non_fiction"]
+    include ActiveModel::Validations
+    validates_with  ClickbaitValidator
 end
