c7e8299407490b1f62d9062bab049682539d6b4f
Caleb Kavanaugh <calebck@hotmail.com>
2019-10-28 00:32:53 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validate :not_clickbait
+  
+  def not_clickbait
+    clickbait = [/Won't Believe/, /Secret/, /Top/, /Guess/]
+
+    if clickbait.none? {|cb| cb.match title}
+      errors.add(:title, "Please use a more clickbait worthy title.")
+    end
+  end
+
 end
+
